251b83d31c518f467a4cb45757f7fd28e68d7b4d
mrapanu <mrapanu@gmail.com>
2024-02-01 11:23:14 +0200

    Added DTOs for Get User Profile and Update User Profile

---
+++ Dtos/GetProfileDto.cs
@@ -0,0 +1,9 @@
+namespace ClimateTrackr_Server.Dtos
+{
+    public class GetProfileDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+    }
+}
