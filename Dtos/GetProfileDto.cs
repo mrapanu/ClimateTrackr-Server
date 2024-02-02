e878d04f324c0b4fcc9708d75606e7e92fb890e6
mrapanu <mrapanu@gmail.com>
2024-02-02 11:20:48 +0200

    Add EnableNotifications for GetProfileDto

--- Dtos/GetProfileDto.cs
+++ Dtos/GetProfileDto.cs
@@ -5,5 +5,6 @@ namespace ClimateTrackr_Server.Dtos
         public string Email { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
+        public bool EnableNotifications { get; set; } 
     }
 }
