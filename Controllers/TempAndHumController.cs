953cc90d1041fb624999e778bf09e2bf3107d588
Mihai Râpanu <mrapanu@gmail.com>
2024-02-13 14:57:07 +0200

    Implement steps for getting data in function of time differences (day / week / month / quarter etc.)

--- Controllers/TempAndHumController.cs
+++ Controllers/TempAndHumController.cs
@@ -1,4 +1,3 @@
-using ClimateTrackr_Server.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -40,7 +39,7 @@ namespace ClimateTrackr_Server.Controllers
                     && entries.Date <= currenttime
                     && entries.Room == room)
                 .FirstOrDefaultAsync();
-            if(response.Data == null)
+            if (response.Data == null)
             {
                 response.Message = "No data available from the sensor!";
                 response.Success = false;
@@ -56,28 +55,53 @@ namespace ClimateTrackr_Server.Controllers
         public async Task<ActionResult<ServiceResponse<IEnumerable<TempAndHum>>>> GetByDate(DateTime timeStart, DateTime timeEnd, string room)
         {
             var response = new ServiceResponse<IEnumerable<TempAndHum>>();
-            //var cacheId = _redisCacheService.Get<IEnumerable<TempAndHum>>($"{start}:{end}:{room}");
+            var timeDifference = timeEnd - timeStart;
+            var step = CalculateStep(timeDifference);
+            response.Data = await GetTempAndHumDataWithStep(timeStart, timeEnd, room, step);
 
-            // if(cacheId != null)
-            // {
-            //     Console.WriteLine("HIT THE CACHE");
-            //     return cacheId;
-            // }
-            // else
-            // {
-            response.Data =
-            await _context.TempAndHums.Where(entries =>
-            timeStart <= entries.Date
-            && entries.Date <= timeEnd && entries.Room == room).ToListAsync();
-            //_redisCacheService.Set($"{start}:{end}:{room}", tempAndHumByDates);
             if (response.Data.Count() == 0)
             {
                 response.Message = "There is no data for this period in the specified room!";
                 return Ok(response);
             }
             return Ok(response);
-            // }
+        }
 
+        private async Task<IEnumerable<TempAndHum>> GetTempAndHumDataWithStep(DateTime timeStart, DateTime timeEnd, string room, TimeSpan step)
+        {
+            var filteredData = await _context.TempAndHums
+                .Where(entry => entry.Room == room && entry.Date >= timeStart && entry.Date <= timeEnd)
+                .ToListAsync();
+
+            var dataWithRowNumber = filteredData
+                .Select((entry, index) => new { Entry = entry, RowNumber = index + 1 })
+                .ToList();
+
+            var data = dataWithRowNumber
+                .Where(x => x.RowNumber % (int)(step.TotalMinutes / 2) == 0)
+                .Select(x => x.Entry);
+
+            return data;
+        }
+
+
+        private TimeSpan CalculateStep(TimeSpan timeDifference)
+        {
+            var stepSizes = new Dictionary<TimeSpan, TimeSpan>
+            {
+                { TimeSpan.FromDays(1), TimeSpan.FromMinutes(2) },
+                { TimeSpan.FromDays(3), TimeSpan.FromMinutes(15) },
+                { TimeSpan.FromDays(7), TimeSpan.FromMinutes(30) },
+                { TimeSpan.FromDays(31), TimeSpan.FromHours(1) },
+                { TimeSpan.FromDays(90), TimeSpan.FromHours(2) },
+                { TimeSpan.MaxValue, TimeSpan.FromHours(6) }
+            };
+            foreach (var kvp in stepSizes)
+            {
+                if (timeDifference < kvp.Key)
+                    return kvp.Value;
+            }
+            return TimeSpan.FromHours(6);
         }
     }
 
